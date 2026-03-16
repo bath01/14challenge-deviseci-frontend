6fb1aa4feaa3fbae4d7ef46d2a2b6b8e24e9141e
alucard1702 <you@example.com>
2026-03-16 11:38:57 +0000

    page devise font

---
+++ src/app/features/devise-ci/navbar/navbar.ts
@@ -0,0 +1,36 @@
+import { CommonModule  } from '@angular/common';
+import { Component,Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+
+  @Input() currentPage = 'converter';
+
+  // dark mode reçu du parent
+  @Input() darkMode = false;
+
+  // événements émis vers le parent
+  @Output() pageChange = new EventEmitter<string>();
+  @Output() darkModeToggle = new EventEmitter<void>();
+
+  navItems = [
+    { key: 'converter', label: 'Convertir' },
+    { key: 'rates', label: 'Taux en direct' },
+    { key: 'history', label: 'Historique' },
+    { key: 'about', label: 'A propos' },
+  ];
+
+  navigate(page: string): void {
+    this.pageChange.emit(page);
+  }
+
+  toggleDark(): void {
+    this.darkModeToggle.emit();
+  }
+
+}
